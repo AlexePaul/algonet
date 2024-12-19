aca336e49bf4c7cb13469395f79cc7760fc2bad5
AlexePaul <alexepaul2011@yahoo.com>
2024-12-19 17:21:58 +0200

    Initial Commit

---
+++ algonetAPI/src/main/java/com/algonet/algonetapi/AlgonetApiApplication.java
@@ -0,0 +1,13 @@
+package com.algonet.algonetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgonetApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgonetApiApplication.class, args);
+    }
+
+}
---
+++ algonetAPI/src/test/java/com/algonet/algonetapi/AlgonetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algonet.algonetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgonetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
