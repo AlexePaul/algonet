f61af109dfd70fc1600e2ad814e4987343a309c5
AlexePaul <alexepaul2011@yahoo.com>
2025-01-13 22:30:15 +0200

    Implemented AuthService Integration Test

---
+++ algonetAPI/src/main/java/com/algonet/algonetapi/models/dto/tagDTOs/TagUpdateDTO.java
@@ -0,0 +1,14 @@
+package com.algonet.algonetapi.models.dto.tagDTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class tagPatchDTO {
+    private String name;
+}
---
+++ algonetAPI/src/test/java/com/algonet/algonetapi/integration/AuthServiceIT.java
@@ -0,0 +1,2 @@
+package com.algonet.algonetapi.integration;public class AuthServiceIT {
+}
